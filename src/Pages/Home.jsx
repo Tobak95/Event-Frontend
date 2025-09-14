0118c381bd2c355ff3ae4f7c91875678ee846410
AA-fullstackdev <adeleke_adeyemi@outlook.com>
2025-09-14 10:06:03 +0100

    How it works and Why Choose Us

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "../component/Hero";
+import HowItWorks from "../component/HowItWorks";
+import WhyChooseUs from "../component/WhyChooseUs";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <HowItWorks/>
+      <WhyChooseUs/>
     </div>
   );
 };
