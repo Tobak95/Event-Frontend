52e41298adf54c553d3956f655e46ac05fa4dce9
osobasamuel <samuelosoba7@gmail.com>
2025-09-14 19:29:58 +0100

    added assests

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-  return (
-    <div>
-      cjcbjbcjcjfjdvhv
-    </div>
-  )
-}
+  return <div>cjcbjbcjcjfjdvhv</div>;
+};
 
-export default Hero
+export default Hero;
