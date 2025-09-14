19d51a0992eb05ec906a7ede880102b217613a04
Aace <aacejovi@gmail.com>
2025-09-14 08:51:28 +0100

    page:contact

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../component/Hero";
+import ContactUs from "./ContactUs";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <ContactUs />
     </div>
   );
 };
