d79c52ed3a6ba71edd85e243b0323fb8ba9138db
Tobak95 <akinola.tobi.2010@gmail.com>
2025-09-15 04:46:14 -0600

    new feat

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Hero from "../component/Hero";
+import UpcomingEvent from "../component/UpcomingEvent";
+import EventCategory from "../component/EventCategory";
 
 const Home = () => {
   return (
     <div>
-      <Hero />
+      <div className="heroBG bg-no-repeat bg-cover w-full bg-bottom ">
+        <Hero />
+      </div>
+      <UpcomingEvent />
+      <EventCategory />
     </div>
   );
 };
