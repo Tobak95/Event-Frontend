042830a7ca275520a72fc249fbdc0a266e5d5925
Tobak95 <akinola.tobi.2010@gmail.com>
2025-10-03 10:44:56 -0600

    updated

--- src/component/HowItWorks.jsx
+++ src/component/HowItWorks.jsx
@@ -22,53 +22,43 @@ const ItWorks = [
 ];
 
 const HowItWorks = () => {
+  return (
+    <main className="bg-[#FFFFFF] lg:mt-20">
+      <article className="layout flex flex-col gap-[47px] ">
+        {/* Header section */}
 
-return (
-
-<main className="bg-[#FFFFFF] lg:mt-20">
-  <article className="layout flex flex-col gap-[47px] ">
-    {/* Header section */}
-
-    <div className="flex flex-col items-center gap-[1px]  ">
-      <h2 className="font-[700] text-[25px] lg:text-[48px] leading-[67px] tracking-[0%]  ">
-        How It Works?
-      </h2>
-      <p className="font-[400] text-[20px] leading-[100%] tracking-[0%] text-[#4A4A4A] ">
-        Discover, book, and enjoy with ease
-      </p>
-    </div>
-
-    {/* Section for How It Works cardS */}
-    <section className="grid gap-[50px] lg:grid-cols-3  h-[350px] ">
-      {ItWorks.map((Work) => {
-        <div
-          key={Work._id}
-          className="card bg-[#FFFFFF] rounded-[12px]  border-[0.4px] border-[#C6C6C6] shadow-sm  shadow-[#B5B5B526]"
-        >
-          <div className="w-[349px] h-[138px] card-body flex mx-auto items-center text-center">
-            <h2 className="bg-[#006F6A] text-white w-[41px] h-[41px] rounded-[80px] items-center text-center  flex justify-center">
-              {Work._id}
-            </h2>
-            <h2 className="card-title font-[500] text-[24px] tracking-[0%] ">
-              {Work.title}
-            </h2>
-            <p className=" text-[#4A4A4A] font-[400] text-[16px]  ">
-              {Work.context}
-            </p>
-          </div>
-
-          <h2 className="card-title font-medium text-lg md:text-xl mb-2">
-            {Work.title}
+        <div className="flex flex-col items-center gap-[1px]  ">
+          <h2 className="font-[700] text-[25px] lg:text-[48px] leading-[67px] tracking-[0%]  ">
+            How It Works?
           </h2>
-
-          <p className="text-[#4A4A4A] font-normal text-sm md:text-base leading-relaxed">
-            {Work.context}
+          <p className="font-[400] text-[20px] leading-[100%] tracking-[0%] text-[#4A4A4A] ">
+            Discover, book, and enjoy with ease
           </p>
-        </div>;
-      })}
-    </section>
-  </article>
-</main> 
-)
-}
+        </div>
+
+        {/* Section for How It Works cardS */}
+        <section className="grid gap-[50px] lg:grid-cols-3  ">
+          {ItWorks.map((Work) => (
+            <div
+              key={Work._id}
+              className="card bg-[#FFFFFF] rounded-[12px] h-[200px] lg:h-[250px]  border-[0.4px] border-[#C6C6C6] shadow-sm  shadow-[#B5B5B526]"
+            >
+              <div className="w-[349px] h-[138px] card-body flex mx-auto items-center text-center">
+                <h2 className="bg-[#006F6A] text-white h-[32px] w-[32px]  lg:w-[41px] lg:h-[41px] rounded-[80px] items-center text-center  flex justify-center">
+                  {Work._id}
+                </h2>
+                <h2 className="card-title font-[500] text-[24px] tracking-[0%] ">
+                  {Work.title}
+                </h2>
+                <p className=" text-[#4A4A4A] font-[400] text-[16px]  ">
+                  {Work.context}
+                </p>
+              </div>
+            </div>
+          ))}
+        </section>
+      </article>
+    </main>
+  );
+};
 export default HowItWorks;
