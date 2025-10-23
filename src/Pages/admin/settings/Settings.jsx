9cacb2ab60a37434db2760c5fedd9b6f67403793
Tobak95 <akinola.tobi.2010@gmail.com>
2025-10-23 00:33:09 -0600

    refactored settings dashboard route

--- src/Pages/admin/settings/Settings.jsx
+++ src/Pages/admin/settings/Settings.jsx
@@ -4,10 +4,55 @@ import Header from "../../../component/common/Header";
 
 const Settings = () => {
   return (
-    <div>
-      
+    <div className="flex h-screen bg-base-200">
+      <SideBar />
+      <div className="flex overflow-hidden flex-col flex-1">
+        <Header />
+        <div className="overflow-y-auto flex-1">
+          <section className="p-7">
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings settings settings
+            settings settings settings settings settings
+          </section>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Settings
